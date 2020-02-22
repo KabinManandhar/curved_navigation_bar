af350b88f7c9a90c862a5f40126ef5b5537928e9
Kabin Manandhar <kobainm666@gmail.com>
2020-02-22 13:00:27 +0545

    Skeumorphic Design

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -111,25 +111,22 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
                   -(1 - _buttonHide) * 80,
                 ),
                 child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 64,
-                      height: 64,
-                      child: _icon,
-                      decoration: BoxDecoration(
-                          color: Color(0xFFF1F2F6),
-                          borderRadius: BorderRadius.circular(32),
-                          boxShadow: [
-                            BoxShadow(
-                                color: Color(0xFFDADFF0),
-                                offset: Offset(8, 6),
-                                blurRadius: 8),
-                            BoxShadow(
-                                color: Colors.white,
-                                offset: Offset(-8, -6),
-                                blurRadius: 8),
-                          ]),
-                    ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: _icon,
+                    decoration: BoxDecoration(
+                        color: Color(0xFFF1F2F6),
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Color(0xFFDADFF0),
+                              offset: Offset(8, 6),
+                              blurRadius: 8),
+                          BoxShadow(
+                              color: Colors.white,
+                              offset: Offset(-8, -6),
+                              blurRadius: 8),
+                        ]),
                   ),
                 ),
               ),
