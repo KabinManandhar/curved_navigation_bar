e479bf74e9217c2dd188f01a95f448d299114d35
Kabin Manandhar <kobainm666@gmail.com>
2020-03-30 17:58:19 +0545

    Soft Button test2

--- lib/curved_navigation_bar.dart
+++ lib/curved_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/src/softButton.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'src/nav_button.dart';
@@ -111,7 +112,7 @@ class CurvedNavigationBarState extends State<CurvedNavigationBar>
                   0,
                   -(1 - _buttonHide) * 80,
                 ),
-                child: Button(
+                child: SoftButton(
                   icon: _icon,
                 ),
               ),
